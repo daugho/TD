03b9be1a2fd404544990b81bc156abd63588930b
sh98923 <sh98923@naver.com>
2025-06-03 12:12:43 +0900

    ui

---
+++ Assets/Script/LSH/StageInfoUI.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StageInfoUI : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _stageInfoUI;
+    [SerializeField]
+    private Button _startButton;
+
+    private void Start()
+    {
+        if(_stageInfoUI != null)
+        {
+            _stageInfoUI.SetActive(false);
+        }
+    }
+    public void ShowStageInfo()
+    {
+        if (_stageInfoUI == null) return;
+
+        _stageInfoUI.SetActive(true);
+    }
+
+    public void CloseStageInfo()
+    {
+        if(_stageInfoUI != null)
+        {
+            _stageInfoUI.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Script/LSH/StageStar.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StageStar : MonoBehaviour
+{
+    [SerializeField]
+    private Image[] _stars;
+    [SerializeField]
+    private Sprite _yellowStar;
+    [SerializeField]
+    private Sprite _grayStar;
+
+    private int _curStarCount = 0;
+
+    private void Start()
+    {
+        ResetStars();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            AddStar();
+        }
+    }
+
+    private void ResetStars()
+    {
+        foreach (Image star in _stars)
+        {
+            star.sprite = _grayStar;
+        }
+
+        _curStarCount = 0;
+    }
+
+    private void AddStar()
+    {
+        if (_curStarCount < _stars.Length)
+        {
+            _stars[_curStarCount].sprite = _yellowStar;
+            _curStarCount++;
+        }
+    }
+}
