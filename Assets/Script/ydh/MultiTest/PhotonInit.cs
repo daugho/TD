7928298cdead972c6f17b2171df6a93d7655b88c
cooljyum <yhj116@naver.com>
2025-06-04 12:04:56 +0900

    ydh00

---
+++ Assets/Script/ydh/MultiTest/PhotonInit.cs
@@ -0,0 +1,11 @@
+using Photon.Pun;
+using UnityEngine;
+
+public class PhotonInit : MonoBehaviour
+{
+    private void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+        Debug.Log("[PhotonInit] 씬 동기화 설정 완료");
+    }
+}
