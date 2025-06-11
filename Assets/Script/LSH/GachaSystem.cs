ed509a3c9d63f6ad410564d4f229cfc2677c5ebc
sh98923 <sh98923@naver.com>
2025-06-11 12:06:19 +0900

    .

--- Assets/Script/LSH/GachaSystem.cs
+++ Assets/Script/LSH/GachaSystem.cs
@@ -1,16 +1,46 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GachaSystem : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    private Sprite circleSprite;
+    [SerializeField]
+    private Sprite triangleSprite;
+    [SerializeField]
+    private Sprite squareSprite;
+    [SerializeField]
+    private Sprite starSprite;
+
+    [SerializeField]
+    private Transform content; // 보관함(Content)
+    [SerializeField]
+    private GameObject shapeButtonPrefab; // 버튼 Prefab
+
+    public void OnGachaButtonClick()
     {
-        
+        int randomValue = Random.Range(1, 101);
+        Sprite selectedSprite = null;
+
+        if (randomValue <= 40)
+            selectedSprite = circleSprite;
+        else if (randomValue <= 70)
+            selectedSprite = triangleSprite;
+        else if (randomValue <= 90)
+            selectedSprite = squareSprite;
+        else
+            selectedSprite = starSprite;
+
+        CreateShapeButton(selectedSprite);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CreateShapeButton(Sprite shapeSprite)
     {
-        
+        // 버튼 생성
+        GameObject newButton = Instantiate(shapeButtonPrefab, content);
+
+        // 버튼의 이미지 설정
+        Image buttonImage = newButton.GetComponent<Image>();
+        buttonImage.sprite = shapeSprite;
     }
 }
