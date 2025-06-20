08fb9a3f1ac71b2dcd0db6a1e4b16460ab8a649e
sh98923 <sh98923@naver.com>
2025-06-20 12:19:21 +0900

    .

--- Assets/Script/LSH/ObjectInteractionUI.cs
+++ Assets/Script/LSH/ObjectInteractionUI.cs
@@ -6,22 +6,26 @@ public class ObjectInteractionUI : MonoBehaviour
     private GameObject interactionUIPrefab; // UI Prefab
     private GameObject activeUI;
 
+    [SerializeField] private Transform _towerUICanvas;
+
     public void OnObjectClick()
     {
         if (activeUI != null)
         {
-            Destroy(activeUI); // 기존 UI 제거
+            //Destroy(activeUI); // 기존 UI 제거
         }
 
-        // UI 생성
-        activeUI = Instantiate(interactionUIPrefab, transform.position, Quaternion.identity);
-
+        if (!interactionUIPrefab.gameObject.activeInHierarchy)
+        {
+            // UI를 활성화
+            interactionUIPrefab.gameObject.SetActive(true);
+        }
         // UI 위치 조정 (오른쪽 위로 오프셋)
-        Vector3 offset = new Vector3(1.0f, 1.0f, 0); // 오른쪽 위로 이동
-        activeUI.transform.position = transform.position + offset;
-
-        // 부모 설정 (월드 공간 유지)
-        activeUI.transform.SetParent(null, true);
+        //Vector3 offset = new Vector3(1.0f, 1.0f, 0); // 오른쪽 위로 이동
+        //activeUI.transform.position = transform.position + offset;
+        //
+        //// 부모 설정 (월드 공간 유지)
+        //activeUI.transform.SetParent(null, true);
     }
 
     public void CloseUI()
