27adf19440a0a3af4e947caf68534c0161e54d22
nyj504 <dydwn980511@gmail.com>
2025-06-26 12:18:12 +0900

    nyj
    nyj

---
+++ Assets/Script/NYJ/GameResult.cs
@@ -0,0 +1,20 @@
+using TMPro;
+using UnityEngine;
+
+public class GameResult : MonoBehaviour
+{
+    private Transform _gameResultPanel;
+
+    private TextMeshProUGUI _remainLifeText;
+    private TextMeshProUGUI _consumeGoldText;
+    private TextMeshProUGUI _huntCountText;
+    private TextMeshProUGUI _builtCountText;
+
+    private int _remainLife;
+    private int _consumeGold;
+    private int _huntCount;
+    private int _buildCount;
+
+
+  
+}
