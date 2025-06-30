19b4d9c469c7ee5b88a361496aeda34c4e78cb4f
cooljyum <yhj116@naver.com>
2025-06-30 11:43:17 +0900

    fix

--- Assets/Script/ydh/MouseShaderSync.cs
+++ Assets/Script/ydh/MouseShaderSync.cs
@@ -6,7 +6,23 @@ public class MouseShaderSync : MonoBehaviour
 
     void Update()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Vector3 screenPos = Vector3.zero;
+        bool hasInput = false;
+
+#if UNITY_EDITOR || UNITY_STANDALONE
+        screenPos = Input.mousePosition;
+        hasInput = true;
+#elif UNITY_ANDROID || UNITY_IOS
+        if (Input.touchCount > 0)
+        {
+            screenPos = Input.GetTouch(0).position;
+            hasInput = true;
+        }
+#endif
+
+        if (!hasInput) return;
+
+        Ray ray = Camera.main.ScreenPointToRay(screenPos);
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
             Shader.SetGlobalVector("_MouseWorldPos", hit.point);
